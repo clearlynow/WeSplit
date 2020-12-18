68bce700517b36a12fcda009df7de9956fe5ad94
clearlynow <59779189+clearlynow@users.noreply.github.com>
2020-12-18 10:19:24 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alison Gorman on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
